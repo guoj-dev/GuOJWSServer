8bad47fa424b3ede98b2f95c1720a8e31855b9ea
Quank123Wip <Quank123Wip@outlook.com>
2019-10-13 15:18:22 +0800

    Added Socket.IO Support

--- socket/index.js
+++ socket/index.js
@@ -1,5 +1,8 @@
 var socket = (socket) => {
-    
+    console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 }
 
 module.exports = socket
